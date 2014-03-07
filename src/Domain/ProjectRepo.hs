4eca52529f51ebff603704800d3f2836f55618cc
AntoineSavage <antoine.savage@gmail.com>
2014-03-07 17:15:14 -0500

    Dat project repo

---
+++ src/Domain/ProjectRepo.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE TypeFamilies #-}
+module Domain.ProjectRepo
+( ProjectRepo ()
+, makeProjectSubmitRepo
+, makeProjectTrainFileRepo
+, makeProjectTrainRunRepo
+) where
+import Infrastructure.Node
+
+import Domain.SubmitRepo
+import Domain.TrainFileRepo
+import Domain.TrainRunRepo
+import Domain.TrainRun
+
+data ProjectRepo = S SubmitRepo | TF TrainFileRepo | TR TrainRunRepo deriving (Show,Eq)
+
+instance HasNode ProjectRepo where
+ type ChildType ProjectRepo = TrainRun
+ 
+ getName (S r) = getName r
+ getName (TF r) = getName r
+ getName (TR r) = getName r
+ 
+ getChildren (S _) = []
+ getChildren (TF _) = []
+ getChildren (TR r) = getChildren r
+ 
+ getChild (S _) _ = Nothing
+ getChild (TF _) _ = Nothing
+ getChild (TR r) s = getChild r s
+
+ toNode (S r) = toNode r
+ toNode (TF r) = toNode r
+ toNode (TR r) = toNode r
+ 
+ fromNode = error "ProjectRepo::fromNode: Cannot decide which project repo to make from raw node" 
+ 
+makeProjectSubmitRepo = S
+makeProjectTrainFileRepo = TF
+makeProjectTrainRunRepo = TR
